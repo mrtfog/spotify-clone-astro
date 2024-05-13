d3e4f1247312aa12e55810583c0286b4fd530703
mrtfog <fogliaccomartin3@gmail.com>
2024-05-13 18:34:23 -0300

    first commit

---
+++ src/components/Greenting.svelte
@@ -0,0 +1,18 @@
+<script lang="es">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours();
+
+    let greeting = ""
+
+    if(currentHour < 12){
+        greeting = "Good Morning";
+    } else if ( currentHour < 18 ) {
+        greeting = "Good Afternoon";
+    } else {
+        greeting = "Good evening";
+    }
+</script>
+
+<h1 class="text-4xl font-bold">
+    {greeting}
+</h1>
